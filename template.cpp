0905ba19afe21d9bdfabd3dfe100b84219ead542
nsunderland1 <sunderland.nicholas@protonmail.com>
2020-12-01 14:45:45 -0500

    Template file

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+  ifstream input("input");
+}
