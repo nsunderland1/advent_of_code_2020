d8bcec0a3ea0d4c85337a81741db10b6608f85d2
nsunderland1 <sunderland.nicholas@protonmail.com>
2020-12-03 17:42:20 -0500

    More template still

--- template.cpp
+++ template.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 #define rep(it,st,en) for(ll it=(st);it<(ll)(en);++it)
 #define allof(c) (c).begin(), (c).end()
-
-const bool IGNORE_EMPTY_LINES = true;
-
+#define allof(c) (c).begin(), (c).end()
+#define mt make_tuple
+#define mp make_pair
+#define pb push_back
+#define X first
+#define Y second
 using ll = int64_t;
 using ld = long double;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using pii = pair<ll, ll>;
+const ll INF=(ll)1e9; 
+const double EPS=(ld)1e-7;
+
+const bool IGNORE_EMPTY_LINES = true;
 
 int main() {
   ifstream in("input");
