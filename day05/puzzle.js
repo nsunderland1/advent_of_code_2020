10963965d19199c45c9c3aa796d0d68f95e44584
nsunderland1 <sunderland.nicholas@protonmail.com>
2020-12-05 01:13:50 -0500

    Could've done this more cleverly just by converting to binary

---
+++ day05/puzzle.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const IGNORE_EMPTY_LINES = true;
+
+const data = fs.readFileSync('input', 'UTF-8');
+let lines = data.split(/\r?\n/);
+
+if (IGNORE_EMPTY_LINES) {
+  lines = lines.filter(line => line !== "");
+}
+
+const id = code => {
+  const v = code.slice(0, 7);
+  const h = code.slice(7, code.length);
+  return (
+      8 * v.split('').reduce((acc, chr) => chr === 'F' ? acc*2 : acc*2+1, 0)
+    + h.split('').reduce((acc, chr) => chr === 'L' ? acc*2 : acc*2+1, 0)
+  );
+}
+
+let prev;
+lines
+  .sort((a, b) => {
+    const [aV, bV] = [a, b].map(str => str.slice(0, 7));
+    const [aH, bH] = [a, b].map(str => str.slice(7, str.length));
+    if (a === b) {
+      return 0;
+    }
+    if (aV === bV) {
+      return aH < bH ? 1 : -1;
+    }
+    return aV < bV ? -1 : 1;
+  })
+  .map(id)
+  .forEach((seatid, i) => {
+    if (i !== 0 && seatid === prev-2) {
+      console.log(seatid+1);
+    }
+    prev = seatid;
+  })
